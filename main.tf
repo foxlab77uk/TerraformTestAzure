6e74d82b126157c545c61a869825bd2a5e415edd
Rusty Fox <russellfox@gmail.com>
2022-02-12 11:44:17 +0000

    update resource group tag

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account" "foxlabsa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "foxlabenv1"
+    environment = "foxlabenvprod1"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
